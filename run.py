c5f64ae31903517ecf43ae80eada9be2ba6bda0d
JakePennell <jake.pennell3@gmail.com>
2022-04-13 14:57:15 +0000

    Add sales ata to sales worksheet

--- run.py
+++ run.py
@@ -37,7 +37,8 @@ def validate_data(values):
     """
     Inside the try, converts all string values into intergers.
     Raises ValueError if strings cannot be converted into int, 
-    or if there aren't exactly 6 values.
+    or if there aren't exactly 6 values. The loop will repeatidly
+    request data until it is valid.
     """
     print(values)
     try: 
@@ -52,4 +53,15 @@ def validate_data(values):
 
     return True
 
+def update_sales_worksheet(data):
+    """
+    Update sales worksheet, add new row with the list data provided.
+    """
+    print("Updating sales worksheet...\n")
+    sales_worksheet = SHEET.worksheet("sales")
+    sales_worksheet.append_row(data)
+    print("Sales worksheet updated successfully.\n")
+
 data = get_sales_data()
+sales_data = [int(num) for num in data]
+update_sales_worksheet(sales_data)
